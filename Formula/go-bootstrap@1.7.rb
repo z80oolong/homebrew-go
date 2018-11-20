7c31e44cbafc004ca4afa731f15732797866b4f8
NAKATSUKA, Yukitaka <zool@zool.jpn.org>
2018-11-21 06:29:57 +0900

    Add Formula/go{-bootstreap@1.7,1.11}.rb

---
+++ Formula/go-bootstrap@1.7.rb
@@ -0,0 +1,30 @@
+class GoBootstrapAT17 < Formula
+  desc "Golang bootstrap compiler for Golang compiler."
+  homepage "https://golang.org"
+
+  if Hardware::CPU.is_32_bit? then
+    case Hardware::CPU.arch_32_bit
+    when :arm
+      url "https://storage.googleapis.com/golang/go1.7.linux-armv6l.tar.gz"
+      sha256 "4192592728e2f9fac8ae43abedb4b98d811836c3965035e7cb8c603aa5e65be4"
+    when :intel
+      url "https://storage.googleapis.com/golang/go1.7.linux-386.tar.gz"
+      sha256 "1207477aa3471222f0555825f9d6ac2a39abc75839f2dfd357f19f5077f710f2"
+    end
+  else
+    if OS.mac?
+      url "https://storage.googleapis.com/golang/go1.7.darwin-amd64.tar.gz"                                                                       
+      sha256 "51d905e0b43b3d0ed41aaf23e19001ab4bc3f96c3ca134b48f7892485fc52961"                                                                   
+    elsif OS.linux?
+      url "https://storage.googleapis.com/golang/go1.7.linux-amd64.tar.gz"                                                                        
+      sha256 "702ad90f705365227e902b42d91dd1a40e48ca7f67a2f4b2fd052aaa4295cd95"                                                                   
+    end
+  end
+  version "1.7"
+
+  keg_only :versioned_formula
+  
+  def install
+    Pathname.glob(buildpath/"*") {|path| (libexec/"go-bootstrap").install path}
+  end
+end
---
+++ Formula/go@1.11.rb
@@ -0,0 +1,103 @@
+class GoAT111 < Formula
+  desc "Open source programming language to build simple/reliable/efficient software"
+  homepage "https://golang.org"
+  version "1.11.1"
+  
+  url "https://dl.google.com/go/go#{version}.src.tar.gz"
+  mirror "https://fossies.org/linux/misc/go#{version}.src.tar.gz"
+  sha256 "558f8c169ae215e25b81421596e8de7572bd3ba824b79add22fba6e284db1117"
+
+  go_version = version.to_s.split(".")[0..1].join(".")
+  resource "gotools" do
+    url "https://go.googlesource.com/tools.git",
+        :branch => "release-branch.go#{go_version}"
+  end
+
+  keg_only :versioned_formula
+
+  depends_on "z80oolong/go/go-bootstrap@1.7"
+
+  option "without-cgo", "Build without cgo (also disables race detector)"
+  option "without-race", "Build without race detector"
+
+  def install
+    # Fixes: Error: Failure while executing: ../bin/ldd ../line-clang.elf: Permission denied
+    unless OS.mac?
+      chmod "+x", Dir.glob("src/debug/dwarf/testdata/*.elf")
+      chmod "+x", Dir.glob("src/debug/elf/testdata/*-exec")
+    end
+
+    if Hardware::CPU.is_32_bit? then
+      case Hardware::CPU.arch_32_bit
+      when :arm
+        ENV["GOHOSTARCH"] = ENV["GOARCH"] = "arm"
+      when :intel
+        ENV["GOHOSTARCH"] = ENV["GOARCH"] = "386"
+      end
+    end
+
+    ENV["GOROOT_BOOTSTRAP"] = "#{Formula["z80oolong/go/go-bootstrap@1.7"].opt_libexec}/go-bootstrap"
+
+    cd "src" do
+      ENV["GOROOT_FINAL"] = libexec
+      ENV["GOOS"]         = OS.mac? ? "darwin" : "linux"
+      ENV["CGO_ENABLED"]  = "0" if build.without?("cgo")
+      system "./make.bash", "--no-clean"
+    end
+
+    (buildpath/"pkg/obj").rmtree
+    libexec.install Dir["*"]
+    bin.install_symlink Dir[libexec/"bin/go*"]
+
+    # Race detector only supported on amd64 platforms.
+    # https://golang.org/doc/articles/race_detector.html
+    if build.with?("cgo") && build.with?("race") && MacOS.prefer_64_bit?
+      system bin/"go", "install", "-race", "std"
+    end
+
+    # Build and install godoc
+    ENV.prepend_path "PATH", bin
+    ENV["GOPATH"] = buildpath
+    (buildpath/"src/golang.org/x/tools").install resource("gotools")
+    cd "src/golang.org/x/tools/cmd/godoc/" do
+      system "go", "build"
+      (libexec/"bin").install "godoc"
+    end
+    bin.install_symlink libexec/"bin/godoc"
+  end
+
+  def caveats; <<~EOS
+    A valid GOPATH is required to use the `go get` command.
+    If $GOPATH is not specified, $HOME/go will be used by default:
+      https://golang.org/doc/code.html#GOPATH
+
+    You may wish to add the GOROOT-based install location to your PATH:
+      export PATH=$PATH:#{opt_libexec}/bin
+  EOS
+  end
+
+  test do
+    (testpath/"hello.go").write <<~EOS
+      package main
+
+      import "fmt"
+
+      func main() {
+          fmt.Println("Hello World")
+      }
+    EOS
+    # Run go fmt check for no errors then run the program.
+    # This is a a bare minimum of go working as it uses fmt, build, and run.
+    system bin/"go", "fmt", "hello.go"
+    assert_equal "Hello World\n", shell_output("#{bin}/go run hello.go")
+
+    # godoc was installed
+    assert_predicate libexec/"bin/godoc", :exist?
+    assert_predicate libexec/"bin/godoc", :executable?
+
+    if build.with? "cgo"
+      ENV["GOOS"] = "freebsd"
+      system bin/"go", "build", "hello.go"
+    end
+  end
+end
